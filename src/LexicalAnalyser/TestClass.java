e6f22fd0ca4783dc3deea9474a49e42cb881f41e
Aidan <daultimatepotato@gmail.com>
2023-03-31 17:24:51 -0500

    initialized project

---
+++ src/LexicalAnalyser/TestClass.java
@@ -0,0 +1,12 @@
+/**
+ * This class tests the functionality of Lexer.
+ *
+ * @author AidanEiler
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        Lexer.Tokenize("YOUR_FILE_HERE");
+    }
+
+}
